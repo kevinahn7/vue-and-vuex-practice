4901989b65801f7b6733d218f5cb9dc6e06caacf
Kevin Ahn <kevinahn7@hotmail.com>
2018-10-19 10:41:26 -0700

    Add a second component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,16 +9,20 @@
       </ul>
     </div>
     <div class="right">
-
+      <stats />
     </div>
   </div>
 </template>
 
 <script>
+import Stats from '@/components/Stats.vue'
 import { mapState } from 'vuex'
 
 export default {
   name: 'HelloWorld',
+  components: {
+    Stats
+  },
   computed: {
     ...mapState([
       'title',
